59aee0e66e02f4aa45de177006646532649e706b
Arjun <arjunofficial08@gmail.com>
2020-11-06 00:48:40 +0530

    Day2Practice

---
+++ JavaApp1_introSelf/src/javaapp1_introself/JavaApp1_introSelf.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp1_introself;
+
+/**
+ *
+ * @author Arjun Singh Negi
+ */
+public class JavaApp1_introSelf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "Arjun Singh Negi";
+        int age = 23;
+        double desiredSalary = 4500000;
+        char gender = 'M';
+        boolean fridayToday = true;
+      
+    }
+    
+    
+}
